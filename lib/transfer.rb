d13c67730e42dcf442fb2074caadb3d254061ebe
Laura Felce <crepu83@yahoo.it>
2018-12-17 09:50:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,8 +18,8 @@ class Transfer
     @receiver.balance += amount
     @status = "complete"
     if !valid?
-    @status = "rejected"
     "Transaction rejected. Please check your account balance."
+    @status = "rejected"
     else
 
     end

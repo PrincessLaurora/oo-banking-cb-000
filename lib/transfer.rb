815ff45b5d522c5711b64233d76216381da435e3
Laura Felce <crepu83@yahoo.it>
2018-12-17 09:45:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
     @sender.balance -= amount
     @receiver.balance += amount
     @status = "complete"
-    else !valid?
+    else 
     "Transaction rejected. Please check your account balance."
     end
   end
